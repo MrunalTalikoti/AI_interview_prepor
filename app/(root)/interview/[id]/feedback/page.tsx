b180c0485968f330a4b5b449537d51ad64d3c225
MrunalTalikoti <mrunaltalikoti2@gmail.com>
2025-09-05 17:12:44 +0530

    final 3

--- app/(root)/interview/[id]/feedback/page.tsx
+++ app/(root)/interview/[id]/feedback/page.tsx
@@ -1,22 +1,5 @@
 import React from 'react'
-import {getCurrentUser} from "@/lib/actions/auth.action";
-import {getFeedbackByInterviewId, getInterviewsById} from "@/lib/actions/general.action";
-import {redirect} from "next/navigation";
 
-const Page = ({ params } : Routeparams) => {
-    const {id} = await params;
-    const user = await getCurrentUser();
-
-    const interview = await getInterviewsById(id);
-    if (!interview) redirect('/');
-
-    const feedback = await getFeedbackByInterviewId({
-        interviewId,
-        userId: user?.id!,
-
-    });
-
-    console.log(feedback);
 
 
     return (
--- lib/actions/general.action.ts
+++ lib/actions/general.action.ts
@@ -106,35 +106,35 @@ export async function createFeedback(params: CreateFeedbackParams) {
     }
 }
 
-export async function getFeedbackByInterviewId(params: GetFeedbackByInterviewIdParams): Promise<Feedback | null> {
-    const {interviewId,userId } = params;
-
-        // If no userId, just return latest finalized interviews
-    const feedback = await db
-        .collection('feedback')
-        .where('interviewId', '==', interviewId)
-        .where('userId', '==', userId)
-        .limit(1)
-        .get();
-
-    if(feedback.empty) return null;
-
-    const feedbackDocs = feedback.docs[0];
-    return {
-        id: feedbackDocs.id, ... feedbackDocs.data()
-    } as Feedback;
-
-    const interviews = await db
-        .collection('interviews')
-        .where('finalized', '==', true)
-        .where('userId', '!=', userId )
-        .orderBy('userId')
-        .orderBy('createdAt', 'desc')
-        .limit(limit)
-        .get();
-
-    return interviews.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data()
-    })) as Interview[];
-}
+// export async function getFeedbackByInterviewId(params: GetFeedbackByInterviewIdParams): Promise<Feedback | null> {
+//     const {interviewId,userId } = params;
+//
+//         // If no userId, just return latest finalized interviews
+//     const feedback = await db
+//         .collection('feedback')
+//         .where('interviewId', '==', interviewId)
+//         .where('userId', '==', userId)
+//         .limit(1)
+//         .get();
+//
+//     if(feedback.empty) return null;
+//
+//     const feedbackDocs = feedback.docs[0];
+//     return {
+//         id: feedbackDocs.id, ... feedbackDocs.data()
+//     } as Feedback;
+//
+//     const interviews = await db
+//         .collection('interviews')
+//         .where('finalized', '==', true)
+//         .where('userId', '!=', userId )
+//         .orderBy('userId')
+//         .orderBy('createdAt', 'desc')
+//         .limit(limit)
+//         .get();
+//
+//     return interviews.docs.map((doc) => ({
+//         id: doc.id,
+//         ...doc.data()
+//     })) as Interview[];
+// }
